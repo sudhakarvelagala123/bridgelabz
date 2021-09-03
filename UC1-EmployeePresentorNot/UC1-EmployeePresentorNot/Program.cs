4fffee0ff2d10536333bb29ad9986241c598a5d3
vijay <sudhakarreddy789@gmail.com>
2021-09-03 14:49:46 +0700

    UC1 is added

---
+++ UC1-EmployeePresentorNot/UC1-EmployeePresentorNot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC1_EmployeePresentorNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
