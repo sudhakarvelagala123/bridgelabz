cfbf569969129b006aaae361f3d4ec4f6f6dc728
vijay <sudhakarreddy789@gmail.com>
2021-09-03 15:31:44 +0700

    UC3-AddPartTimeEmployee

---
+++ UC3-AddPartTimeEmployee/UC3-AddPartTimeEmployee/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC3_AddPartTimeEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage:" + empWage);
+        }
+}
