9301fb80b061debb1084204211dca493c6b9c8a9
sudhakarvelagala123 <88834979+sudhakarvelagala123@users.noreply.github.com>
2021-09-06 01:11:09 +0700

    Update Program.cs

--- UC5-CalculateWagesMonthly/UC5-CalculateWagesMonthly/Program.cs
+++ UC5-CalculateWagesMonthly/UC5-CalculateWagesMonthly/Program.cs
@@ -4,6 +4,9 @@ namespace UC5_CalculateWagesMonthly
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;   
         static void Main(string[] args)
         {
             //Variables
